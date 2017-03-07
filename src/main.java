3d8735447a17093cd02eae6aae1c48cfb99f2be5
rmit-s3444615-ryan-teo <s3444615@student.rmit.edu.au>
2017-03-07 19:07:01 +1100

    Julia Push

--- src/main.java
+++ src/main.java
@@ -1,8 +1,15 @@
+import java.util.*;
 
 public class Main {
 	
 	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
 		System.out.println("--Welcome to ABC Booking system--");
+		System.out.println("1 : Login");
+		System.out.println("2 : Register");
+		System.out.println("3 : Quit");
+		String userInput = scan.nextLine();
+		
 	}
 	
 }

1471fe092c01486a8956301773b4a4207b329c26
rmit-s3510376-Harry-Utomo <s3510376@student.rmit.edu.au>
2017-03-19 23:15:51 +1100

    "Develop business and booking class"

--- src/Booking.java
+++ src/Booking.java
@@ -1,24 +1,19 @@
 import java.util.HashMap;
 
 public class Booking {
-private String business_name;
 private String time;
 private Customer cust;
 private Employee emp;
 
-Booking(String name,String time,Customer cust, Employee emp){
-	this.business_name = name;
+Booking(String time,Customer cust, Employee emp){
 	this.time = time;
 	this.cust = cust;
 	this.emp = emp;
 }
 
-public String getBusiness_name() {
-	return business_name;
-}
-
-public void setBusiness_name(String business_name) {
-	this.business_name = business_name;
+Booking(String name,String time, Employee emp){
+	this.time = time;
+	this.emp = emp;
 }
 
 public String getTime() {
--- src/BookingTest.java
+++ src/BookingTest.java
@@ -7,22 +7,18 @@ import org.junit.Test;
 public class BookingTest {
 	Customer customer = new Customer("Bob", "Bobber", "Bobby123", "Bob@bob.com", "999999999");;
 	Employee emp = new Employee("Harry","Monday,afternoon");
-	Booking book_1 = new Booking("ABC","Monday,afternoon",customer,emp);
+	Booking book_1 = new Booking("Monday,afternoon",customer,emp);
 		
 		@Test
 		public void initialising(){
 			assertNotNull("should instantiate booking", book_1);
-			assertTrue(book_1.getBusiness_name() instanceof String);
 			assertTrue(book_1.getTime() instanceof String);
 			assertTrue(book_1.getCust() instanceof Customer);
 			assertTrue(book_1.getEmp() instanceof Employee);
 		}
 		
-		@Test void testBusinessName(){
-			
-		}
 		
-		@Test void testCustomerName(){
+		@Test void makeBooking(){
 			
 		}
 		

7448b971ce6b1365e25e924303297db49e97922f
Anthony Orn <s3383336@student.rmit.edu.au>
2017-03-20 21:43:05 +1100

    fillied in business test class

--- src/BusinessTest.java
+++ src/BusinessTest.java
@@ -1,12 +1,36 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 
 public class BusinessTest {
-
+	
+	//list of employees
+	//list of times
+	
+	Business business = new Business("Bob inc.", "Bobb",null ,null );
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitialise() {
+		assertTrue(business.getName() instanceof String);
+		assertTrue(business.getOwner() instanceof String);
+		assertTrue(business.getEmp() instanceof ArrayList);
+		assertTrue(business.getSession() instanceof ArrayList);
 	}
-
+	
+	@Test
+	public void testName(){
+		Pattern nameP = Pattern.compile("[a-zA-Z][a-z]*");
+		Matcher nameM = nameP.matcher(business.getName());
+		assertTrue(nameM.matches());
+	}
+	
+	@Test
+	public void testOwner(){
+		Pattern nameP = Pattern.compile("[a-zA-Z][a-z]*");
+		Matcher nameM = nameP.matcher(business.getOwner());
+		assertTrue(nameM.matches());
+	}
 }

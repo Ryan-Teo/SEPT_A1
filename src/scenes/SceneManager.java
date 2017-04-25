2daf0c09f96a30ef68ad2e1c6554dac17efb7fd5
Anthony Orn <s3383336@student.rmit.edu.au>
2017-04-25 19:42:18 +1000

    merged with showsession

--- src/scenes/SceneManager.java
+++ src/scenes/SceneManager.java
@@ -25,37 +25,96 @@ import users.User;
 
 public class SceneManager {
 	Stage window;
-	Scene mainMenu, customerRegister, registerMenu, customerMenu, custSelectBus, custSelectSession;
+	Scene mainMenu, customerRegister, registerMenu, customerMenu, custSelectBus, custSelectSession, customerBookingSummary;
 	ArrayList<Customer> customers;
 	ArrayList<Business> businesses;
 	User userInst = null;
 	Account acct;
 	int busIndex;
-	
-	
-	public SceneManager(ArrayList<Customer> customers,	ArrayList<Business> businesses, Account account, Stage primaryStage){
+
+	public SceneManager(ArrayList<Customer> customers, ArrayList<Business> businesses, Account account,
+			Stage primaryStage) {
 		this.customers = customers;
 		this.businesses = businesses;
 		this.acct = account;
 		window = primaryStage;
 	}
-	
-	public void mainLogIn(ArrayList<Customer> customers, ArrayList<Business> businesses, String username, String password){
+
+	public void mainLogIn(ArrayList<Customer> customers, ArrayList<Business> businesses, String username,
+			String password) {
 		userInst = acct.login(customers, businesses, username, password);
 	}
-	public void mainRegisterCust(ArrayList<Customer> customers, String name, String username, String password1, String password2, String phone, String address){
-		if(acct.registerCustomer(name, username, password1, password2, phone, address, customers)){
+
+	public void mainRegisterCust(ArrayList<Customer> customers, String name, String username, String password1,
+			String password2, String phone, String address) {
+		if (acct.registerCustomer(name, username, password1, password2, phone, address, customers)) {
 			/*
-			 * If the registration is successful, then show a pop up box.
-			 * The pop up box will tell the user that they have successfully registered.
-			 * The pop up box needs to have a button in it.
-			 * After the user clicks on the button, return them to the main menu scene.
+			 * If the registration is successful, then show a pop up box. The
+			 * pop up box will tell the user that they have successfully
+			 * registered. The pop up box needs to have a button in it. After
+			 * the user clicks on the button, return them to the main menu
+			 * scene.
 			 */
-			
+
 			System.out.println("successful");
-		
-			
+
 		}
+
+	}
+
+	public void showBookingSummary() {
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(30, 30, 30, 30));
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+
+		Text header = new Text("Your Summary");
+		header.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
+		grid.add(header, 0, 1, 2, 1);
+		
+		
+
+		
+
+
+		
+		
+		
+		
+		Button backToMenuButton = new Button("Go back to menu");
+		HBox hbBackToMenuButton = new HBox(10);
+		hbBackToMenuButton.setAlignment(Pos.BOTTOM_RIGHT);
+		backToMenuButton.minHeight(50);
+		backToMenuButton.minWidth(100);
+		backToMenuButton.setStyle("-fx-font: 22 arial; -fx-base: #000555;");
+		hbBackToMenuButton.getChildren().add(backToMenuButton);
+		grid.add(hbBackToMenuButton, 1, 5);
+
+		backToMenuButton.setOnAction(e -> {
+			customerMenu();
+			window.setScene(customerMenu);
+		});
+		
+		customerBookingSummary = new Scene(grid,600, 250);
+		
+		
+		
+		Button backToMenuButton = new Button("Go back to menu");
+		HBox hbBackToMenuButton = new HBox(10);
+		hbBackToMenuButton.setAlignment(Pos.BOTTOM_RIGHT);
+		backToMenuButton.minHeight(50);
+		backToMenuButton.minWidth(100);
+		backToMenuButton.setStyle("-fx-font: 22 arial; -fx-base: #000555;");
+		hbBackToMenuButton.getChildren().add(backToMenuButton);
+		grid.add(hbBackToMenuButton, 1, 5);
+
+		backToMenuButton.setOnAction(e -> {
+			customerMenu();
+			window.setScene(customerMenu);
+		});
+		
+		customerBookingSummary = new Scene(grid,600, 250);
 		
 	}
 	public void selectTime(){
@@ -101,201 +160,196 @@ public class SceneManager {
         });
         custSelectBus = new Scene(grid, 500, 500);
 	}
-	
-	public void showMainMenu(){
-    	GridPane grid = new GridPane();
-    	grid.setPadding(new Insets(30, 30, 30, 30));
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-       
-        Text header = new Text("Welcome to Press The Button");
-        header.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
-        grid.add(header, 0, 1, 2, 1);
 
-        Label userName = new Label("User Name:");
-        grid.add(userName, 0, 2);
+	public void showMainMenu() {
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(30, 30, 30, 30));
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
 
-        TextField userNameInput = new TextField();
-        userNameInput.setPromptText("username");
-        grid.add(userNameInput, 1, 2);
-        
+		Text header = new Text("Welcome to Press The Button");
+		header.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
+		grid.add(header, 0, 1, 2, 1);
 
-        Label password = new Label("Password:");
-        grid.add(password, 0, 3);
+		Label userName = new Label("User Name:");
+		grid.add(userName, 0, 2);
 
-        PasswordField passwordInput = new PasswordField();
-        passwordInput.setPromptText("password");
-        grid.add(passwordInput, 1, 3);
-        
+		TextField userNameInput = new TextField();
+		userNameInput.setPromptText("username");
+		grid.add(userNameInput, 1, 2);
 
-        
-        Button signInButton = new Button("Sign in");
-        HBox hbsignInButton = new HBox(10);
-        hbsignInButton.setAlignment(Pos.BOTTOM_RIGHT);
-        signInButton.setMinWidth(100);
-        signInButton.setMinHeight(50);
-        signInButton.setStyle("-fx-font: 22 arial; -fx-base: #000555;");
-        hbsignInButton.getChildren().add(signInButton);
-        grid.add(hbsignInButton, 1, 5);
-        
-        signInButton.setOnAction(e -> {
-        	String userNameString = userNameInput.getText();
-        	String passwordString = passwordInput.getText();
-        	mainLogIn(customers, businesses, userNameString, passwordString);
-        	
-        	
-        	if(userInst instanceof Customer){
-        		customerMenu();
-        		window.setScene(customerMenu);
-        	}
-        });
+		Label password = new Label("Password:");
+		grid.add(password, 0, 3);
 
-        Button registerButton = new Button("Click Here to Register");
-        HBox hbRegisterButton = new HBox(10);
-        hbRegisterButton.setAlignment(Pos.TOP_RIGHT);
-        registerButton.setMinWidth(100);
-        registerButton.setMinHeight(25);
-        registerButton.setStyle("-fx-font: 10 arial; -fx-base: #009900;");
-        hbRegisterButton.getChildren().add(registerButton);
-        grid.add(hbRegisterButton, 1, 0);
-        registerButton.setOnAction(e -> {
-	        	showRegister();
-	        	window.setScene(registerMenu);
-        	});
-        mainMenu = new Scene(grid, 600, 250);
+		PasswordField passwordInput = new PasswordField();
+		passwordInput.setPromptText("password");
+		grid.add(passwordInput, 1, 3);
+
+		Button signInButton = new Button("Sign in");
+		HBox hbsignInButton = new HBox(10);
+		hbsignInButton.setAlignment(Pos.BOTTOM_RIGHT);
+		signInButton.setMinWidth(100);
+		signInButton.setMinHeight(50);
+		signInButton.setStyle("-fx-font: 22 arial; -fx-base: #000555;");
+		hbsignInButton.getChildren().add(signInButton);
+		grid.add(hbsignInButton, 1, 5);
+
+		signInButton.setOnAction(e -> {
+			String userNameString = userNameInput.getText();
+			String passwordString = passwordInput.getText();
+			mainLogIn(customers, businesses, userNameString, passwordString);
+
+			if (userInst instanceof Customer) {
+				customerMenu();
+				window.setScene(customerMenu);
+			}
+		});
+
+		Button registerButton = new Button("Click Here to Register");
+		HBox hbRegisterButton = new HBox(10);
+		hbRegisterButton.setAlignment(Pos.TOP_RIGHT);
+		registerButton.setMinWidth(100);
+		registerButton.setMinHeight(25);
+		registerButton.setStyle("-fx-font: 10 arial; -fx-base: #009900;");
+		hbRegisterButton.getChildren().add(registerButton);
+		grid.add(hbRegisterButton, 1, 0);
+		registerButton.setOnAction(e -> {
+			showRegister();
+			window.setScene(registerMenu);
+		});
+		mainMenu = new Scene(grid, 600, 250);
 	}
-	
-	public void showRegister(){
-        GridPane grid2a = new GridPane();
-        grid2a.setPadding(new Insets(30, 30, 30, 30));
-        grid2a.setAlignment(Pos.CENTER);
-        grid2a.setHgap(10);
-        grid2a.setVgap(10);
-        
-        Text registerheader = new Text("Are you registering as:");
-        registerheader.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
-        grid2a.add(registerheader, 0, 1, 1, 1);
-        
-        Button custInstButton = new Button("Customer");
-        HBox hbcustInstButton = new HBox(10);
-        custInstButton.setMinWidth(100);
-        custInstButton.setMinHeight(50);
-        custInstButton.setStyle("-fx-font: 22 arial; -fx-base: #000555;");
-        hbcustInstButton.getChildren().add(custInstButton);
-        grid2a.add(hbcustInstButton, 0, 2);
-        custInstButton.setOnAction(e -> {
-        	registerCustomer();
-        	window.setScene(customerRegister);
-        });
-        
-        Button ownerInstButton = new Button("Business");
-        HBox hbOwnerInstButton = new HBox(10);
-        ownerInstButton.setMinWidth(100);
-        ownerInstButton.setMinHeight(50);
-        ownerInstButton.setStyle("-fx-font: 22 arial; -fx-base: #000555;");
-        hbOwnerInstButton.getChildren().add(ownerInstButton);
-        grid2a.add(hbOwnerInstButton, 1, 2);
-        ownerInstButton.setOnAction(e -> {
-        		//meant to show the business menu
-        		showMainMenu();
-        		window.setScene(mainMenu);
-        	});
-        
-        
-        registerMenu = new Scene(grid2a, 600, 250);
+
+	public void showRegister() {
+		GridPane grid2a = new GridPane();
+		grid2a.setPadding(new Insets(30, 30, 30, 30));
+		grid2a.setAlignment(Pos.CENTER);
+		grid2a.setHgap(10);
+		grid2a.setVgap(10);
+
+		Text registerheader = new Text("Are you registering as:");
+		registerheader.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
+		grid2a.add(registerheader, 0, 1, 1, 1);
+
+		Button custInstButton = new Button("Customer");
+		HBox hbcustInstButton = new HBox(10);
+		custInstButton.setMinWidth(100);
+		custInstButton.setMinHeight(50);
+		custInstButton.setStyle("-fx-font: 22 arial; -fx-base: #000555;");
+		hbcustInstButton.getChildren().add(custInstButton);
+		grid2a.add(hbcustInstButton, 0, 2);
+		custInstButton.setOnAction(e -> {
+			registerCustomer();
+			window.setScene(customerRegister);
+		});
+
+		Button ownerInstButton = new Button("Business");
+		HBox hbOwnerInstButton = new HBox(10);
+		ownerInstButton.setMinWidth(100);
+		ownerInstButton.setMinHeight(50);
+		ownerInstButton.setStyle("-fx-font: 22 arial; -fx-base: #000555;");
+		hbOwnerInstButton.getChildren().add(ownerInstButton);
+		grid2a.add(hbOwnerInstButton, 1, 2);
+		ownerInstButton.setOnAction(e -> {
+			// meant to show the business menu
+			showMainMenu();
+			window.setScene(mainMenu);
+		});
+
+		registerMenu = new Scene(grid2a, 600, 250);
 	}
-	
+
 	public void registerCustomer() {
-        GridPane grid2 = new GridPane();
-    	grid2.setPadding(new Insets(30, 30, 30, 30));
-    	grid2.setAlignment(Pos.CENTER);
-    	grid2.setHgap(10);
-    	grid2.setVgap(10);
-    	
-        
-        Text registerTitle = new Text("Register Here:");
-        registerTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
-        grid2.add(registerTitle, 0, 1, 2, 1);
-        
-        Label fullName = new Label("Full Name: ");
-        grid2.add(fullName, 0, 2);
+		GridPane grid2 = new GridPane();
+		grid2.setPadding(new Insets(30, 30, 30, 30));
+		grid2.setAlignment(Pos.CENTER);
+		grid2.setHgap(10);
+		grid2.setVgap(10);
 
-        TextField fullNameText = new TextField();
-        fullNameText.setPromptText("legal name");
-        grid2.add(fullNameText, 1, 2);
+		Text registerTitle = new Text("Register Here:");
+		registerTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
+		grid2.add(registerTitle, 0, 1, 2, 1);
 
-        Label newUserName = new Label("User Name:");
-        grid2.add(newUserName, 0, 3);
+		Label fullName = new Label("Full Name: ");
+		grid2.add(fullName, 0, 2);
 
-        TextField newUserNameInput = new TextField();
-        newUserNameInput.setPromptText("username");
-        grid2.add(newUserNameInput, 1, 3);
+		TextField fullNameText = new TextField();
+		fullNameText.setPromptText("legal name");
+		grid2.add(fullNameText, 1, 2);
 
-        Label newPassword = new Label("Password:");
-        grid2.add(newPassword, 0, 4);
+		Label newUserName = new Label("User Name:");
+		grid2.add(newUserName, 0, 3);
 
-        PasswordField newPasswordInput = new PasswordField();
-        newPasswordInput.setPromptText("password");
-        grid2.add(newPasswordInput, 1, 4);
-        
-        Label newPassword2 = new Label("Re-Enter Password:");
-        grid2.add(newPassword2, 0, 5);
+		TextField newUserNameInput = new TextField();
+		newUserNameInput.setPromptText("username");
+		grid2.add(newUserNameInput, 1, 3);
 
-        PasswordField newPasswordInput2 = new PasswordField();
-        newPasswordInput2.setPromptText("password");
-        grid2.add(newPasswordInput2, 1, 5);
-        
-        Label phone = new Label("Contact Number ");
-        grid2.add(phone, 0, 6);
+		Label newPassword = new Label("Password:");
+		grid2.add(newPassword, 0, 4);
 
-        TextField phoneText = new TextField();
-        phoneText.setPromptText("phone number");
-        grid2.add(phoneText, 1, 6);
-        
-        Label address = new Label("Address ");
-        grid2.add(address, 0, 7);
+		PasswordField newPasswordInput = new PasswordField();
+		newPasswordInput.setPromptText("password");
+		grid2.add(newPasswordInput, 1, 4);
 
-        TextField addressNew = new TextField();
-        addressNew.setPromptText("address");
-        grid2.add(addressNew, 1, 7);
-        
-        Button register = new Button("Register!");
-        HBox hbRegister = new HBox(10);
-        hbRegister.setAlignment(Pos.BOTTOM_RIGHT);
-        register.setMinWidth(80);
-        register.setMinHeight(50);
-        register.setStyle("-fx-font: 22 arial; -fx-base: #009900;");
-        hbRegister.getChildren().add(register);
-        grid2.add(hbRegister, 1, 8);
-        
-        register.setOnAction(e -> {
-        	String fullNameString = fullNameText.getText();
-        	String newUserNameString = newUserNameInput.getText();
-        	String newPasswordString = newPasswordInput.getText();
-        	String newPasswordString2 = newPasswordInput2.getText();
-        	String phoneString = phoneText.getText();
-        	String addressString = addressNew.getText();
-        	mainRegisterCust(customers, fullNameString, newUserNameString, newPasswordString, newPasswordString2, phoneString, addressString);
-        	showMainMenu();
-        	window.setScene(mainMenu);
-        	
-        });
-            
-        Button back = new Button("Back");
-        HBox hbBack = new HBox(10);
-        hbBack.setAlignment(Pos.TOP_RIGHT);
-        back.setMinWidth(50);
-        back.setMinHeight(25);
-        back.setStyle("-fx-font: 10 arial; -fx-base: #000555;");
-        hbBack.getChildren().add(back);
-        grid2.add(hbBack, 1, 0);
-        back.setOnAction(e -> {
-        	showMainMenu();
-        	window.setScene(mainMenu);
-        });
-        
-        customerRegister = new Scene(grid2, 200, 400);
+		Label newPassword2 = new Label("Re-Enter Password:");
+		grid2.add(newPassword2, 0, 5);
+
+		PasswordField newPasswordInput2 = new PasswordField();
+		newPasswordInput2.setPromptText("password");
+		grid2.add(newPasswordInput2, 1, 5);
+
+		Label phone = new Label("Contact Number ");
+		grid2.add(phone, 0, 6);
+
+		TextField phoneText = new TextField();
+		phoneText.setPromptText("phone number");
+		grid2.add(phoneText, 1, 6);
+
+		Label address = new Label("Address ");
+		grid2.add(address, 0, 7);
+
+		TextField addressNew = new TextField();
+		addressNew.setPromptText("address");
+		grid2.add(addressNew, 1, 7);
+
+		Button register = new Button("Register!");
+		HBox hbRegister = new HBox(10);
+		hbRegister.setAlignment(Pos.BOTTOM_RIGHT);
+		register.setMinWidth(80);
+		register.setMinHeight(50);
+		register.setStyle("-fx-font: 22 arial; -fx-base: #009900;");
+		hbRegister.getChildren().add(register);
+		grid2.add(hbRegister, 1, 8);
+
+		register.setOnAction(e -> {
+			String fullNameString = fullNameText.getText();
+			String newUserNameString = newUserNameInput.getText();
+			String newPasswordString = newPasswordInput.getText();
+			String newPasswordString2 = newPasswordInput2.getText();
+			String phoneString = phoneText.getText();
+			String addressString = addressNew.getText();
+			mainRegisterCust(customers, fullNameString, newUserNameString, newPasswordString, newPasswordString2,
+					phoneString, addressString);
+			showMainMenu();
+			window.setScene(mainMenu);
+
+		});
+
+		Button back = new Button("Back");
+		HBox hbBack = new HBox(10);
+		hbBack.setAlignment(Pos.TOP_RIGHT);
+		back.setMinWidth(50);
+		back.setMinHeight(25);
+		back.setStyle("-fx-font: 10 arial; -fx-base: #000555;");
+		hbBack.getChildren().add(back);
+		grid2.add(hbBack, 1, 0);
+		back.setOnAction(e -> {
+			showMainMenu();
+			window.setScene(mainMenu);
+		});
+
+		customerRegister = new Scene(grid2, 200, 400);
 	}
 	
 	public void customerMenu(){
@@ -333,6 +387,10 @@ public class SceneManager {
         custCurrent.setStyle("-fx-font: 10 arial; -fx-base: #000555;");
         hbCustCurrent.getChildren().add(custCurrent);
         grid3.add(hbCustCurrent, 2, 0);
+		custCurrent.setOnAction(e -> {
+			showBookingSummary();
+			window.setScene(customerBookingSummary);
+		});
         
         Button custSession = new Button("View Available Sessions");
         HBox hbCustSession = new HBox(10);
@@ -369,11 +427,11 @@ public class SceneManager {
         
         customerMenu = new Scene(grid3, 200, 400);
 	}
-	
-	public void show(){
-        window.setMinHeight(300);
-        window.setMinWidth(600);
-        window.setScene(mainMenu);
-        window.show();
+
+	public void show() {
+		window.setMinHeight(300);
+		window.setMinWidth(600);
+		window.setScene(mainMenu);
+		window.show();
 	}
 }

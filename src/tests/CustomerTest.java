b44c6915734f5d9c5c3db5bb72d8bdfb58ffa8bc
rmit-s3510376-Harry-Utomo <s3510376@student.rmit.edu.au>
2017-05-24 11:03:16 +1000

    pushing

--- src/tests/CustomerTest.java
+++ src/tests/CustomerTest.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -33,7 +34,7 @@ import users.Employee;
 public class CustomerTest {
 	FileIO FIO = new FileIO();
 	Customer customer = new Customer("Bob", "Bobber", "Bobby123", "Bob@bob.com", "999999999");
-	Business myBusiness = new Business("Sal's Hair Salon","Harry","Alumbra St","0120103004","harryOwner","password", null, null, null);
+	Business myBusiness = new Business("Sal's Hair Salon","Harry","Alumbra St","0120103004","harryOwner","password", "09:00", "17:00", "15");
 	ArrayList<Business> businesses = new ArrayList<Business>();
 	Employee emp = new Employee("james","james",myBusiness);
 	LinkedHashMap<LocalDate,Booking[]> linkedHm = new LinkedHashMap<LocalDate, Booking[]>();
@@ -41,9 +42,11 @@ public class CustomerTest {
 	DateTimeFormatter daytimeformat = DateTimeFormatter.ofPattern("EE");
 	DateTimeFormatter timeformat = DateTimeFormatter.ofPattern("HH:mm");
 	LocalDate myDate = LocalDate.now();
+	HashMap<LocalDate, HashMap<LocalTime, Boolean>> schedule = new HashMap<LocalDate, HashMap<LocalTime, Boolean>>() ; //one month in advance
 	LocalTime myTime = LocalTime.parse("09:00", timeformat);
 	Scanner scan = new Scanner(System.in);
 	ArrayList<Booking> myBooking = new ArrayList<Booking>();
+	
 	
 	
 	@Test
@@ -57,53 +60,10 @@ public class CustomerTest {
 		assertTrue(customer.getPhone() instanceof String);
 	}
 	
-	@Test
-	public void testName(){
-		//perform regex on customer name
-		Pattern nameP = Pattern.compile("[a-zA-Z][a-z]*");
-		Matcher nameM = nameP.matcher(customer.getName());
-		assertTrue(nameM.matches());
-		
-	}
-	
-	@Test
-	public void testPassword(){
-		//perform regex for password
-		//for length of password (6-15)
-		Pattern passwordP = Pattern.compile("\\w*{6,15}");
-		Matcher passwordM = passwordP.matcher(customer.getPassword());
-		assertTrue(passwordM.matches());
-	}
-	
-	@Test
-	public void testUser(){
-		//limit the length of the username (6-15)
-		Pattern userP = Pattern.compile("\\w*{6,15}");
-		Matcher userM = userP.matcher(customer.getUsername());
-		assertTrue(userM.matches());
-	}
-	
-	@Test
-	public void testAddress(){
-		//regex for email address
-		Pattern addressP = Pattern.compile("\\w*.com");
-		Matcher addressM = addressP.matcher(customer.getAddress());
-		assertTrue(addressM.matches());
-	}
-	
-	@Test
-	public void testPhone(){
-		//regex for length of phone number, and that it is all numbers
-		Pattern phoneP = Pattern.compile("[0-9]{9}");
-		Matcher phoneM = phoneP.matcher(customer.getPhone());
-		assertTrue(phoneM.matches());
-		
-	}
-	
-
 	
 	@Test 
 	public void testMakeBooking(){
+		myBusiness.addService("male hair cut", 15);
 		Boolean value = false;
 		value = customer.makeBooking(myDate, myTime, customer, myBusiness, emp, "male hair cut", myBooking);
 	
@@ -130,6 +90,7 @@ public class CustomerTest {
 	
 	@Test
 	public void testCancelBooking(){
+		myBusiness.addService("male hair cut", 15);
 		Boolean value = false;
 		value = customer.makeBooking(myDate, myTime, customer, myBusiness, emp, "male hair cut", myBooking);
 		value = customer.cancelBooking(myBooking, myBooking.get(0));
